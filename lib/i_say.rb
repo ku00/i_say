db5524105ca02f9b047b0b5aae709c609d1211a8
takuminnnn <skentaro36@gmail.com>
2015-05-01 10:20:16 +0900

    add say command

--- lib/i_say.rb
+++ lib/i_say.rb
@@ -1,9 +1,21 @@
 require "i_say/version"
 require "i_say/config"
 require "i_say/cli"
+require 'voice_text_api'
 
 module ISay
   class << self
-    # Your code goes here...
+    SPEAKER = %i(haruka hikari takeru)
+    VOLUME  = 200
+
+    def fetch_sound(word, config)
+      # return unless word
+      raise "ENV['VT_API_KEY'] is required" unless config.api_key
+
+      voice_text = VoiceTextAPI.new(config.api_key)
+      speaker = SPEAKER[rand(3)]
+
+      voice_text.tts(word, speaker, volume: VOLUME)
+    end
   end
 end
--- lib/i_say/cli.rb
+++ lib/i_say/cli.rb
@@ -1,11 +1,17 @@
 require "i_say"
 require "thor"
+require 'open3'
 
 module ISay
   class CLI < Thor
-    desc "hoge", "print thor test"
-    def hoge
-      puts "Hello thor..."
+    default_command :say
+
+    desc "say", "say word"
+    def say(word)
+      config = ISay::Config.new
+
+      sound = ISay.fetch_sound(word, config)
+      Open3.capture3("/usr/local/bin/play -", :stdin_data => sound)
     end
   end
 end
